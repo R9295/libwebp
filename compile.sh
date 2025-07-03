c2ba535ca9f335a49bbdb09638d6f90fabf6504f
aarnav <aarnavbos@gmail.com>
2025-07-03 19:18:08 +0200

    done

--- compile.sh
+++ compile.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 [ -d "build" ] && rm -rf "build"
 
-#if [ "$1" = "asan" ]; then
-CMAKE_C_FLAGS="-fsanitize=address" CMAKE_CXX_FLAGS="-fsantitize=address" CC=afl-clang-fast CXX=afl-clang-fast++ cmake -B ./build S .
-CMAKE_C_FLAGS="-fsanitize=address" CMAKE_CXX_FLAGS="-fsantitize=address" CC=afl-clang-fast CXX=afl-clang-fast++ make -j16 -C ./build
+if [ "$1" = "asan" ]; then
+CC=afl-clang-fast CXX=afl-clang-fast++ cmake -DCMAKE_C_FLAGS="-fsanitize=address -fsanitize=undefined" -DCMAKE_CXX_FLAGS="-fsantitize=address -fsanitize=undefined" -S . -B ./build
+CC=afl-clang-fast CXX=afl-clang-fast++ make -j16 -C ./build
 afl-clang-fast++ -I. -o decode-asan decode.cc build/libwebp.a build/libwebpdecoder.a -fsanitize=fuzzer,address,undefined
-
 else
-CMAKE_C_FLAGS="-fsanitize=address" CMAKE_CXX_FLAGS="-fsantitize=address" CC=afl-clang-fast CXX=afl-clang-fast++ cmake -B ./build S .
-CMAKE_C_FLAGS="-fsanitize=address" CMAKE_CXX_FLAGS="-fsantitize=address" CC=afl-clang-fast CXX=afl-clang-fast++ make -j16 -C ./build
-afl-clang-fast++ -I. -o decode-asan decode.cc build/libwebp.a build/libwebpdecoder.a -fsanitize=fuzzer
+ CC=afl-clang-fast CXX=afl-clang-fast++ cmake -B ./build S .
+ CC=afl-clang-fast CXX=afl-clang-fast++ make -j16 -C ./build
+afl-clang-fast++ -I. -o decode decode.cc build/libwebp.a build/libwebpdecoder.a -fsanitize=fuzzer
 fi
